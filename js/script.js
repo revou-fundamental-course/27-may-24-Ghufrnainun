e20bde25cb51bc55b0e4407200c6eca6e82d6f94
Ghufrnainun <ghufrnainunajib@gmail.com>
2024-06-05 21:34:18 +0700

    Add image slides

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+// slide image
+const sliderItems = document.querySelectorAll(".slider-item");
+
+let sliderActive = 1;
+
+if (sliderItems) {
+  sliderItems.forEach((slider, index) => {
+    if (index === 0) {
+      slider.setAttribute("data-show", "show");
+    } else {
+      slider.setAttribute("data-show", "hidden");
+    }
+  });
+
+  setInterval(() => {
+    sliderItems.forEach((slider, index) => {
+      if (sliderActive === index) {
+        slider.setAttribute("data-show", "show");
+      } else {
+        slider.setAttribute("data-show", "hidden");
+      }
+    });
+
+    if (sliderActive === sliderItems.length - 1) {
+      sliderActive = 0;
+    } else {
+      sliderActive++;
+    }
+  }, 4000);
+}
+
+// nama
+function replaceName() {
+  let name = prompt("Siapakah nama anda?", "");
+  document.getElementById("name").innerHTML = name;
+}
+
+replaceName();
+
+// form
 function validateForm() {
   const name = document.forms["message-form"]["full-name"].value;
   const birthDate = document.forms["message-form"]["birth-date"].value;
